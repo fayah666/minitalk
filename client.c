8f9ac4c86bf8b3801f352c793b16ea5b9632f07b
hfandres <hfandres@hfandres.hfandres>
2025-07-17 09:22:13 -0400

    add function send_strlen and adding buffer to receve msg

--- client.c
+++ client.c
@@ -3,29 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hfandres <hfandres@student.42antananari    +#+  +:+       +#+        */
+/*   By: hfandres <hfandres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:43:43 by hfandres          #+#    #+#             */
-/*   Updated: 2025/07/14 15:11:35 by hfandres         ###   ########.fr       */
+/*   Updated: 2025/07/16 13:08:18 by hfandres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <signal.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void	send_strlen(int pid, int len)
+{
+	int	i;
+
+	i = 0;
+	while (i < 32)
+	{
+		if ((len >> (31 - i)) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		i++;
+	}
+}
+
+void	send_char(int pid, char c)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if ((c >> (7 - i)) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		i++;
+	}
+}
 
 int	main(int ac, char **av)
 {
+	char	*str;
+	int		len;
+
 	if (ac < 2 || ac > 4)
 	{
 		ft_printf("USAGE : %s <SERVER_PID> <string>\n", av[0]);
 		return (0);
-		
 	}
 	else
 	{
-		ft_printf ("send %s to %s", av[1], av[2]);
-		kill (atoi(av[1]), atoi(av[2]));	
+		str = av[2];
+		len = ft_strlen(str);
+		send_strlen(atoi(av[1]), len);
+		while (*str)
+		{
+			send_char(atoi(av[1]), *str);
+			str++;
+		}
+		send_char (atoi(av[1]), '\0');
 	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,35 +1,68 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hfandres <hfandres@student.42antananari    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/14 14:23:18 by hfandres          #+#    #+#             */
-/*   Updated: 2025/07/14 15:31:28 by hfandres         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_printf.h"
 #include <unistd.h>
 #include <signal.h>
 
-void	get_sig(int sig)
+static int g_msg_len = 0;
+
+static int handle_len(int sig)
 {
-	if (sig == 2)
-	{
-		ft_printf ("yu get SIGINT [%d]\n", sig);
-		exit (0);
-	}
-}
-int	main(void)
-{
-	ft_printf("PID : %d\n", getpid());
-	signal (2, get_sig);
-	while (1)
-	{
-        write(STDOUT_FILENO, "En attente...\n", 14);
-        sleep(5);
+    static int bit = 0;
+    if (bit == 0)
+        g_msg_len = 0;
+    if (sig == SIGUSR1)
+        g_msg_len |= (1 << (31 - bit));
+    bit++;
+    if (bit == 32)
+    {
+        ft_printf("Message length: %d\n", g_msg_len);
+        bit = 0;
+        return (1);
     }
-	return (0);
+    return (0);
+}
+
+static void handle_msg(int sig)
+{
+    static int bit = 0;
+    static char c = 0;
+    static char msg[1000] = {0}; // Assuming a max message length of 1000
+    static int i = 0;
+
+    
+    if (sig == SIGUSR1)
+        c |= (1 << (7 - bit));
+    bit++;
+    if (bit == 8)
+    {
+        msg[i++] = c;
+        if (c == '\0' || i == g_msg_len)
+        {
+            ft_printf("%s\n", msg);
+            g_msg_len = 0;
+            i = 0;
+            bit = 0;
+            c = 0;
+        }
+        bit = 0;
+        c = 0;
+    }
+}
+
+void handle_signal(int sig)
+{
+    static int receiving_len = 1;
+    if (receiving_len)
+        receiving_len = !handle_len(sig);
+    else
+        handle_msg(sig);
+}
+
+int main(void)
+{
+    ft_printf("PID : %d\n", getpid());
+    signal(SIGUSR1, handle_signal);
+    signal(SIGUSR2, handle_signal);
+    while (1)
+        pause();
+    return (0);
 }
